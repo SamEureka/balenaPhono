1f9f189ef3a51c995c978e7a30d3c8cef686d306
Sam Dennon <sameureka@me.com>
2021-12-20 12:28:36 -0800

    changed the distro for phono and booter

--- balenaPhono/start.sh
+++ balenaPhono/start.sh
@@ -7,7 +7,8 @@ cp icecast.xml /etc/icecast2/
 service icecast2 start
 sleep 2
 
-until [ -d /proc/asound/card1 ]
+## You may need to change this to /proc/asound/card1 ##
+until [ -d /proc/asound/card0 ]
 do
   echo "Waiting for USB sound device."
   sleep 5
