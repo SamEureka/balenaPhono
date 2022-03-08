f2c3120041fa5d673f56855ebfcce2efd111b556
Sam Dennon <sameureka@me.com>
2022-03-08 05:12:01 -0800

    Update start.sh

--- balenaPhono/start.sh
+++ balenaPhono/start.sh
@@ -1,11 +1,16 @@
 #/bin/bash
 
+## balenaPhono/start.sh ##
+## Sam Dennon//2021     ##
+
+# Build the config files
 python3 phonoConfig.py
 
 echo "Copying icecast2 service and config."
 cp icecast2 /etc/default/
 cp icecast.xml /etc/icecast2/
 
+echo "Starting the Icecast2 service"
 service icecast2 start
 sleep 2
 
